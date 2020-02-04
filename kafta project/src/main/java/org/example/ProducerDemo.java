db7af3707922a41ff67211730b35df4111541462
Ricardo Sotolongo <rickey.soto@gmail.com>
2020-02-04 12:09:14 -0500

    inserted call back with for loop on data..

--- kafta project/src/main/java/org/example/ProducerDemo.java
+++ kafta project/src/main/java/org/example/ProducerDemo.java
@@ -1,13 +1,18 @@
 package org.example;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
 public class ProducerDemo {
 
     public static void main(String[] args) {
+
+        final Logger logger = LoggerFactory.getLogger(ProducerDemoWithCallback.class);
         String bootstrapServers = "127.0.0.1:9092";
        //create Producer properties
         Properties properties = new Properties();
@@ -18,11 +23,25 @@ public class ProducerDemo {
         // create the producer
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
 
-        //create a producer record
-        ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", "hello world");
+        for (int i=0; i<10; i++){
+            //create a producer record
+            ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", "hello world" + Integer.toString(i));
 
-        //send data
-        producer.send(record);
+            //send data
+            producer.send(record, new Callback() {
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    if (e == null) {
+                        logger.info("Received new metadata. \n" +
+                                "Topic: " + recordMetadata.topic() + "\n" +
+                                "Partition: " + recordMetadata.partition() + "\n" + "offset: " + recordMetadata.offset() + "\n" +
+                                "Timestamp" + recordMetadata.timestamp());
+
+                    } else {
+                        logger.error("Error while producing", e);
+                    }
+                }
+            });
+        };
 
         producer.flush();
     }
